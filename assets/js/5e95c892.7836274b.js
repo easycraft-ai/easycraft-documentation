56d9aa9363e874372617a9b58cc01b77cb226588
easycraft-ai <ali@easycraft.ai>
2025-08-20 16:31:34 +0800

    Deploy website - based on 7c3c2387111a71d0e31cfcfd28027c614a17af20

---
+++ assets/js/5e95c892.7836274b.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkeasycraft_documentation=self.webpackChunkeasycraft_documentation||[]).push([[9647],{7121:(e,a,s)=>{s.r(a),s.d(a,{default:()=>d});s(96540);var r=s(34164),t=s(17559),c=s(45500),n=s(22831),u=s(21656),o=s(74848);function d(e){return(0,o.jsx)(c.e3,{className:(0,r.A)(t.G.wrapper.docsPages),children:(0,o.jsx)(u.A,{children:(0,n.v)(e.route.routes)})})}}}]);
